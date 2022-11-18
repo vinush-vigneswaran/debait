86d93bb4912f50c2dc9607e0c255998fd5a87dd3
vkvincent <vinush10@hotmail.co.uk>
2022-11-18 20:05:53 +0000

    added unit tests

---
+++ tests/cohere_api_test.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import pytest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from model import cohere_api
+from model import helper
+
+'''
+Generate:
+    - Test if we get reply from API
+    - Test exception error
+    
+    CMD: pytest -s tests
+'''
+class TestCohereAPI:
+    API_KEY = helper.read_file('app/api_key.txt')
+
+    TRAINING_DATA_DIR = 'prompt_data/training_data.txt'
+    ARTICLE_DIR = 'prompt_data/article.txt'
+    HISTORY_DIR = 'prompt_data/history.txt'
+
+    training_data = helper.read_file(TRAINING_DATA_DIR)
+    article = helper.read_file(ARTICLE_DIR)
+    history = helper.read_file_lines(HISTORY_DIR, lookback=5) #only look back at 5 conversations
+
+    def test_generate_return_string(self):
+        # Test that string reply from API
+        x = "This is a test input"
+        user_input = [TestCohereAPI.article, x, "disagree", "short", ""]
+        prompt = helper.generatePrompt(TestCohereAPI.training_data, TestCohereAPI.history, user_input)
+        _,_,response = cohere_api.generate(prompt, TestCohereAPI.API_KEY)
+        print(response)
+        assert ((type(response) is str) and (len(response) > 2))
+
+    def test_generate_invalid_request(self):
+        # Test for invalid request
+        x = ""
+        status, error_msg, response = cohere_api.generate(x, TestCohereAPI.API_KEY)
+        print(status, error_msg, response)
+        assert ((response == "") and (status==400) and (error_msg=="invalid request: prompt must be at least 1 token long"))
---
+++ tests/unit_test.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from model import helper
+
+class TestClass:
+
+    def test_length_classify_1(self):
+        # Test short output (<=25)
+        x = 'You are correct'
+        assert helper.length_classify(x) == 'short'
+
+    def test_length_classify_2(self):
+        # Test short output (<=50)
+        x = 'You are correct, the principles of thermodynamics indicate that perpetual motion is a ruse'
+        assert helper.length_classify(x) == 'short'
+
+    def test_length_classify_3(self):
+        # Test medium output (<=50)
+        x = "Space is the boundless three-dimensional extent in which objects and events have relative position and direction. In classical physics, physical space is often conceived in three linear dimensions, although modern physicists usually consider it, with time, to be part of a boundless four-dimensional continuum known as spacetime. Blah Blah"
+        assert helper.length_classify(x) == 'medium'
+
+    def test_length_classify_4(self):
+        # Test long output (>50) - 50 words
+        x = "Space is the boundless three-dimensional extent in which objects and events have relative position and direction. In classical physics, physical space is often conceived in three linear dimensions, although modern physicists usually consider it, with time, to be part of a boundless four-dimensional continuum known as spacetime. Blah Blah Blah Blah"
+        assert helper.length_classify(x) == 'long'
