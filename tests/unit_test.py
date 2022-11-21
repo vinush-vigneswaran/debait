07532fe806c1659ff126922780630191d8f9ea5e
vkvincent <vinush10@hotmail.co.uk>
2022-11-21 19:36:24 +0000

    moved from model to app/utils

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,7 +1,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from model import helper
+from app.utils import helper
+
 
 class TestClass:
 
